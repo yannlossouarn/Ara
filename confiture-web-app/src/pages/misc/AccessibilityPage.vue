ebafbe5e13896763588dbd381a5b4221456e1b5a
Yann Lossouarn <yann@lossouarn.net>
2025-02-28 10:51:33 +0100

    Modification de la page Accessibilité avec les informations d'AplusB

--- confiture-web-app/src/pages/misc/AccessibilityPage.vue
+++ confiture-web-app/src/pages/misc/AccessibilityPage.vue
@@ -8,87 +8,40 @@ import PageMeta from "../../components/PageMeta";
 <template>
   <PageMeta
     title="Accessibilité"
-    description="Déclaration d’accessibilité du site ara.numerique.gouv.fr"
+    description="Déclaration d’accessibilité du site aram.aplusb.pro"
   />
   <section class="fr-my-0 fr-mx-auto content">
     <h1>Déclaration d’accessibilité</h1>
     <p>
-      La <strong>direction interministérielle du numérique (DINUM)</strong>
+      AplusB
       s’engage à rendre ses sites internet, intranet, extranet et ses progiciels
       accessibles (et ses applications mobiles et mobilier urbain numérique)
       conformément à l’article 47 de la loi n°2005-102 du 11 février 2005.
     </p>
     <p>
       Cette déclaration d’accessibilité s’applique à
-      <strong>ara.numerique.gouv.fr</strong>.
+      <strong>aram.aplusb.pro</strong>.
     </p>
 
     <h2 id="etat-conformite">État de conformité</h2>
 
     <p>
       Aucun audit n’a été encore effectué.<br />
-      Ara.numerique.gouv.fr est non conforme avec le référentiel général
+      Aram.aplusb.pro est non conforme avec le référentiel général
       d’amélioration de l’accessibilité (RGAA).
     </p>
     <p>
-      Un audit de conformité est bien planifié et l’état de conformité sera mis
-      à jour avant la fin de la version bêta.
+      Un audit de conformité est planifié par la DINUM sur l'outil Ara, dont Aram est un service dérivé. Aram sera mis
+      à jour en conséquence.
     </p>
 
     <h2 id="contact">Retour d’information et contact</h2>
     <p>
       Si vous n’arrivez pas à accéder à un contenu ou à un service, vous pouvez
       nous contacter par courriel à l’adresse
-      <strong>contact@design.numerique.gouv.fr</strong> pour être orienté vers
+      <strong>contact@aplusb.pro</strong> pour être orienté vers
       une alternative accessible ou obtenir le contenu sous une autre forme.
     </p>
-
-    <h2 id="recours">Voies de recours</h2>
-    <p>
-      Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un
-      contenu ou une fonctionnalité du site, que vous nous le signalez et que
-      vous ne parvenez pas à obtenir une réponse rapide de notre part, vous êtes
-      en droit de faire parvenir vos doléances ou une demande de saisine au
-      Défenseur des droits.
-    </p>
-    <p>Plusieurs moyens sont à votre disposition :</p>
-
-    <ul>
-      <li>
-        un
-        <a
-          href="https://www.defenseurdesdroits.fr/nous-contacter"
-          target="_blank"
-          el="noreferrer noopener"
-          >formulaire de contact
-          <span class="fr-sr-only">(nouvelle fenêtre)</span></a
-        >
-      </li>
-      <li>
-        la
-        <a
-          href="https://www.defenseurdesdroits.fr/fr/saisir/delegues"
-          el="noreferrer noopener"
-          target="_blank"
-          >liste des délégués de votre région
-          <span class="fr-sr-only">(nouvelle fenêtre)</span></a
-        >
-        avec leurs informations de contact directs
-      </li>
-      <li>
-        un numéro de téléphone : le +33 (0) 9 69 39 00 00 du lundi au vendredi
-        de 8h30 à 19h30 (coût d’un appel local)
-      </li>
-      <li>
-        une adresse postale dans l’<a
-          href="https://lannuaire.service-public.fr/autorites-independantes/1867f065-c823-4362-8d0f-8ca6b011a10f"
-          rel="noreferrer noopener"
-          target="_blank"
-          >annuaire de l’administration
-          <span class="fr-sr-only">(nouvelle fenêtre)</span></a
-        >
-      </li>
-    </ul>
   </section>
 </template>
 
